f338354be7024c159264418b10310605e338b2f1
swasher <mr.swasher@gmail.com>
2016-10-21 12:28:51 +0000

    Emplement Featherlight for cloudinary pictures

--- inventory/widgets.py
+++ inventory/widgets.py
@@ -6,6 +6,8 @@ import cloudinary
 
 class AdminCloudinaryWidget(AdminFileWidget):
 
+
+
     #template_with_initial = ('%(initial_text)s: <a href="%(initial_url)s">%(initial)s</a> %(clear_template)s<br />%(input_text)s: %(input)s')
     # override:
     template_with_initial = ''
@@ -16,6 +18,7 @@ class AdminCloudinaryWidget(AdminFileWidget):
 
     def render(self, name, value, attrs=None):
         output = []
+        cloudinary_prefix = 'https://cloudinary.com/console/media_library#/dialog/image/upload/'
 
         if value and getattr(value, "url", None):
 
@@ -29,12 +32,13 @@ class AdminCloudinaryWidget(AdminFileWidget):
             except cloudinary.api.NotFound:
                 alt = ''
 
-            picture_preview = cloudinary.CloudinaryImage(value.public_id).image(format='JPG', width = 150, height = 150, crop = 'fill', alt = "Sample Image")
-            picture_full = cloudinary.CloudinaryImage(value.public_id).build_url()
-            picture = '<a href={}  target="_blank" alt="{}">{}</a>'.format(picture_full, alt, picture_preview)
-            cloudinary_link = '<a href="https://cloudinary.com/console/media_library#/dialog/image/upload/{}" target="_blank">Cloudinary Link</a>'.format(value.public_id)
+            picture_preview = cloudinary.CloudinaryImage(value.public_id).image(format='JPG', width = 150, height = 150, crop = 'fill', alt = alt) # html TAG 'a' with a small pict
+            picture_full = cloudinary.CloudinaryImage(value.public_id).build_url()                                                                 # http link to full pict
+            cloudinary_link = '<a href="{}{}" target="_blank">Cloudinary Link</a>'.format(cloudinary_prefix, value.public_id)                      # html TAG 'a' with text link
 
-            output.append('<div>{}</div> <div class="cloudinary-image">{}</div><div>{}</div>'.format(caption, picture, cloudinary_link))
+            output.append('<div>{}</div>'.format(caption))
+            output.append('<a href="{}" data-featherlight="image">{}</a>'.format(picture_full, picture_preview))
+            output.append('<div>{}</div>'.format(cloudinary_link))
 
         output.append(super(AdminFileWidget, self).render(name, value, attrs))
         return mark_safe(u''.join(output))
