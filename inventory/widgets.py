a01ffff2f4fed823923a5eb6685e18e8b54167af
swasher <mr.swasher@gmail.com>
2016-11-02 19:56:34 +0000

    Fix bug when PDF cloudinary files do not open with featherlight.js

--- inventory/widgets.py
+++ inventory/widgets.py
@@ -20,7 +20,12 @@ class AdminCloudinaryWidget(AdminFileWidget):
         output = []
         cloudinary_prefix = 'https://cloudinary.com/console/media_library#/dialog/image/upload/'
 
+
         if value and getattr(value, "url", None):
+            try:
+                file_format = cloudinary.api.resource(value.public_id)['format']
+            except (cloudinary.api.NotFound, AttributeError):
+                file_format = None
 
             try:
                 caption = cloudinary.api.resource(value.public_id)['context']['custom']['caption']
@@ -34,11 +39,26 @@ class AdminCloudinaryWidget(AdminFileWidget):
 
             picture_preview = cloudinary.CloudinaryImage(value.public_id).image(format='JPG', width = 150, height = 150, crop = 'fill', alt = alt) # html TAG 'a' with a small pict
             picture_full = cloudinary.CloudinaryImage(value.public_id).build_url()                                                                 # http link to full pict
+            picture_featherlight = '<a href="{}" data-featherlight="image" class="featherlight-loading">{}</a>'.format(picture_full, picture_preview)
+            picture_simple = '<a href={}  target="_blank" alt="{}">{}</a>'.format(picture_full, alt, picture_preview)
             cloudinary_link = '<a href="{}{}" target="_blank">Cloudinary Link</a>'.format(cloudinary_prefix, value.public_id)                      # html TAG 'a' with text link
 
+            if file_format in ['jpg', 'jpeg', 'png', 'gif']:
+                picture = picture_featherlight
+            else:
+                picture = picture_simple
+
             output.append('<div>{}</div>'.format(caption))
-            output.append('<a href="{}" data-featherlight="image" class="featherlight-loading">{}</a>'.format(picture_full, picture_preview))
+            output.append('<div>{}</div>'.format(picture))
             output.append('<div>{}</div>'.format(cloudinary_link))
 
         output.append(super(AdminFileWidget, self).render(name, value, attrs))
         return mark_safe(u''.join(output))
+
+
+    # picture_preview = cloudinary.CloudinaryImage(value.public_id).image(format='JPG', width = 150, height = 150, crop = 'fill', alt = "Sample Image")
+    # picture_full = cloudinary.CloudinaryImage(value.public_id).build_url()
+    # picture = '<a href={}  target="_blank" alt="{}">{}</a>'.format(picture_full, alt, picture_preview)
+    # cloudinary_link = '<a href="https://cloudinary.com/console/media_library#/dialog/image/upload/{}" target="_blank">Cloudinary Link</a>'.format(value.public_id)
+    #
+    # output.append('<div>{}</div> <div class="cloudinary-image">{}</div><div>{}</div>'.format(caption, picture, cloudinary_link))
